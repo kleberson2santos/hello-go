fe92acee7eaed80b6e3039d0a1e4bf80b6d85605
Kleberson Santos <kleberson2santos@gmail.com>
2021-02-27 18:20:59 -0400

    fazendo requisições web

--- hello.go
+++ hello.go
@@ -1,28 +1,36 @@
 //hello.go
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
 
-	
+	for {
+		exibeMenu()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -40,10 +48,40 @@ func exibeMenu() {
 	fmt.Println("0- Sair do Programa")
 }
 
- func leComando() int {
+func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 
 	fmt.Println("O valor da variável é:", comandoLido)
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := []string{"https://kleberson2santos.github.io/kleberson2santos", "http://k2santos.cf.s3-website-us-east-1.amazonaws.com/"}
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+
+	}
+
+	fmt.Println("")
+
+}
+
+func testaSite(site string) {
+
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas.")
+	}
 }
